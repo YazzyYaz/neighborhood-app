c06dee2538710730066bf387f48ecdf6f0bf43fe
Yaz Khoury <yaz.khoury@gmail.com>
2015-09-25 17:53:08 -0400

    feat: Add search functionality for maps to point to location

--- test/js/app.js
+++ test/js/app.js
@@ -1,4 +1,6 @@
 'use strict';
+
+// Documentation can be found at https://developers.google.com/maps/documentation/javascript/examples/places-searchbox?hl=en
 var map;
 
 function initMap() {
@@ -9,6 +11,60 @@ function initMap() {
 	};
 	map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
+	var input = document.getElementById('keyterm');
+	var searchBox = new google.maps.places.SearchBox(input);
+
+	// Bias the SearchBox results towards current map's viewport.
+	map.addListener('bounds_changed', function() {
+		searchBox.setBounds(map.getBounds());
+	});
+
+	var markers = [];
+
+	searchBox.addListener('places_changed', function() {
+		var places = searchBox.getPlaces();
+
+		if (places.length == 0) {
+			return;
+		}
+
+		// Clear out the old markers.
+		markers.forEach(function(marker) {
+			marker.setMap(null);
+		});
+		
+		markers = [];
+
+		// For each place, get the icon, name and location.
+		var bounds = new google.maps.LatLngBounds();
+		
+		places.forEach(function(place) {
+			var icon = {
+				url: place.icon,
+				size: new google.maps.Size(71, 71),
+				origin: new google.maps.Point(0, 0),
+				anchor: new google.maps.Point(17, 34),
+				scaledSize: new google.maps.Size(25, 25)
+			};
+
+			// Create a marker for each place.
+			markers.push(new google.maps.Marker({
+				map: map,
+				icon: icon,
+				title: place.name,
+				position: place.geometry.location
+			}));
+
+			if (place.geometry.viewport) {
+				// Only geocodes have viewport.
+				bounds.union(place.geometry.viewport);
+			} else {
+				bounds.extend(place.geometry.location);
+			}
+		});
+		map.fitBounds(bounds);
+	});
+	// Add event listener to know if there's a window resize and adapt
 	google.maps.event.addDomListener(window, 'resize', function() {
 		var center = map.getCenter();
 		google.maps.event.trigger(map, 'resize');
@@ -17,4 +73,3 @@ function initMap() {
 }
 
 google.maps.event.addDomListener(window, 'load', initMap);
-
